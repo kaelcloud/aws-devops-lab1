1d46b91ad61a1c6ec9f43b5e1cb34b6a846bc8c6
Muhammad Hazran Hafiz Bin Ahmad <hazran@HPJ.LOCAL>
2026-01-20 11:35:20 +0800

    Update text on website

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello dari AWS ECS Fargate! 🚀</h1>');
+  res.send('<h1>Hello dari AWS ECS Fargate! Automasi berjaya! Welcome to new world of DevOps 🚀</h1>');
 });
 
 app.listen(port, () => {
